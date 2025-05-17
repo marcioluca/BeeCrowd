0dafba0c74ed0dea0162ce938844360e049e472f
marcio lucas <lucasmrc2014@gmail.com>
2025-05-17 08:19:10 -0300

    resolving problem 1070

--- br/com/exercicios/Bee1070.java
+++ br/com/exercicios/Bee1070.java
@@ -6,11 +6,11 @@ public class Bee1070 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         Integer x = s.nextInt();
-        Integer contador = 0;
-        while (contador < 6) {
+        Integer cont = 0;
+        while (cont < 6) {
             if(x % 2 != 0) {
                 System.out.println(x);
-                contador++;
+                cont++;
             }
             x++;
         }
