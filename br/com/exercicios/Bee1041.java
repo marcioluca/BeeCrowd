b6358c905ea9f2d9e23f45e3c7956b67d6483c7e
marcio lucas <lucasmrc2014@gmail.com>
2025-05-18 10:05:40 -0300

    resolving problem 1041

---
+++ br/com/exercicios/Bee1041.java
@@ -0,0 +1,37 @@
+package br.com.exercicios;
+
+
+
+import java.util.Scanner;
+
+public class Bee1041 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double x = s.nextDouble();
+        double y = s.nextDouble();
+
+        if (x == 0 && y == 0) {
+            System.out.println("Origem");
+        }
+
+        else if (x == 0) {
+            System.out.println("Eixo Y");
+        }
+
+        else if (y == 0) {
+            System.out.println("Eixo X");
+        }
+
+        else if (x > 0 && y > 0) {
+            System.out.println("Q1");
+        } else if (x < 0 && y > 0) {
+            System.out.println("Q2");
+        } else if (x < 0 && y < 0) {
+            System.out.println("Q3");
+        } else {
+            System.out.println("Q4");
+        }
+
+        s.close();
+    }
+}
