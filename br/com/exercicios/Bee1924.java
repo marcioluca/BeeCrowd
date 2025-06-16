ff7f1c09b611480c3af5eec879c26130f57e3f6c
marcio lucas <lucasmrc2014@gmail.com>
2025-06-16 08:24:19 -0300

    resolving problem 1924

---
+++ br/com/exercicios/Bee1924.java
@@ -0,0 +1,15 @@
+package br.com.exercicios;
+
+import java.util.Scanner;
+
+public class Bee1924 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String s = "";
+        for (int i = 0; i <= n ; i++) {
+            s = scanner.nextLine();
+        }
+        System.out.println("Ciencia da computacao");
+    }
+}
