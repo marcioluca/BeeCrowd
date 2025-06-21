5f3b61ed3613356118a0b362f0ed5d699b5a8a47
marcio lucas <lucasmrc2014@gmail.com>
2025-06-21 07:03:37 -0300

    resolving problem 1101

--- br/com/exercicios/Bee1101.java
+++ br/com/exercicios/Bee1101.java
@@ -5,27 +5,29 @@ import java.util.Scanner;
 public class Bee1101 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int m = s.nextInt();
-        int n = s.nextInt();
+        int m, n;
+        do{
+            m = s.nextInt();
+            n = s.nextInt();
+            if(m <= 0 || n <= 0) {
+                return;
+            }
+            int sum = 0;
+            int inicio;
+            int fim;
+            if(m > n) {
+                inicio = n;
+                fim = m;
+            } else {
+                inicio = m;
+                fim = n;
+            }
 
-        if(m <= 0 || n <= 0) {
-            return;
+            for (int i = inicio; i <= fim ; i++) {
+                System.out.print(i + " ");
+                sum += i;
+            }
+            System.out.println("Sum=" + sum);
+        } while(m != 0 || n != 0);
         }
-        int sum = 0;
-        int inicio;
-        int fim;
-        if(m > n) {
-            inicio = n;
-            fim = m;
-        } else {
-            inicio = m;
-            fim = n;
-        }
-
-        for (int i = inicio; i <= fim ; i++) {
-            System.out.print(i + " ");
-            sum += i;
-        }
-        System.out.println("Sum=" + sum);
-    }
 }
