c6bedeedd781154d49f7122b641c1910e4dc162c
marcio lucas <lucasmrc2014@gmail.com>
2025-06-23 16:27:28 -0300

    Resolving problem 1150

---
+++ Bee1150.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+// Ultrapassando Z..
+public class Bee1150 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int z;
+
+        do {
+            z = s.nextInt();
+        } while (z <= x);
+
+        int proximo = x + 1;
+        int cont = 1;
+
+        while (x < z) {
+            x += proximo;
+            proximo++;
+            cont++;
+        }
+        System.out.println(cont);
+    }
+}
