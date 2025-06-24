481ce460d671a8dd72b551a89c373efe1b7f2702
marcio lucas <lucasmrc2014@gmail.com>
2025-06-24 06:54:09 -0300

    resolving problem 1150 & 1157

---
+++ br/com/exercicios/Bee1157.java
@@ -0,0 +1,17 @@
+package br.com.exercicios;
+
+import java.util.Scanner;
+
+public class Bee1157 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
