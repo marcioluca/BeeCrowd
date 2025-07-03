89db05288e4da7150b07fd39c5b2bdf1a19647ad
marcio lucas <lucasmrc2014@gmail.com>
2025-07-03 12:21:19 -0300

    resolving problem 1164

---
+++ br/com/exercicios/Bee1164.java
@@ -0,0 +1,35 @@
+package br.com.exercicios;
+
+import java.util.Scanner;
+
+// Número Perfeito
+public class Bee1164 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int casosTeste = sc.nextInt();
+
+        if (casosTeste >= 1 &&  casosTeste <= 100) {
+
+            while (casosTeste > 0) {
+                int numero = sc.nextInt();
+                int divisores = 0;
+
+                for (int i = 1; i < numero ; i++) {
+                    if(numero % i == 0) {
+                        divisores += i;
+                    }
+                }
+                if (divisores == numero) {
+                    System.out.println(numero + " eh perfeito");
+                } else {
+                    System.out.println(numero + " nao eh perfeito");
+                }
+
+                casosTeste--;
+            }
+
+        }
+
+    }
+}
