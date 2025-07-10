d56c55f87c16a0fc09543c937f597487f4ab40ea
marcio lucas <lucasmrc2014@gmail.com>
2025-07-10 07:59:15 -0300

    resolving problem 1182

---
+++ br/com/exercicios/Bee1182.java
@@ -0,0 +1,29 @@
+package br.com.exercicios;
+
+import java.util.Scanner;
+
+public class Bee1182 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        float[][] M = new float[12][12];
+
+        int c = s.nextInt();
+        char T = s.next().charAt(0);
+
+        for(int i = 0; i < 12; i++) {
+            for(int j = 0; j < 12; j++) {
+                M[i][j] = s.nextFloat();
+            }
+        }
+
+        float soma = 0;
+        for (int i = 0; i < 12; i++) {
+            soma+= M[i][c];
+        }
+        if(T == 'S') {
+            System.out.printf("%.1f\n", soma);
+        } else if(T == 'M') {
+            System.out.printf("%.1f\n", soma / 12);
+        }
+    }
+}
