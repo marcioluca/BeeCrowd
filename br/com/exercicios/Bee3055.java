b29bfd068b1fa31a624fe67f2f60ede3f267cd16
marcio lucas <lucasmrc2014@gmail.com>
2025-07-17 10:12:55 -0300

    resolving problem 3055

---
+++ br/com/exercicios/Bee3055.java
@@ -0,0 +1,16 @@
+package br.com.exercicios;
+
+import java.util.Scanner;
+
+public class Bee3055 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int m = sc.nextInt();
+
+        int b = (m * 2) - a;
+
+        System.out.println(b);
+    }
+}
