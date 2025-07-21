270013071a6bf328615558543a8859cb583ba0dc
marcio lucas <lucasmrc2014@gmail.com>
2025-07-21 13:10:19 -0300

    resolving problem 2752

---
+++ br/com/exercicios/Bee2752.java
@@ -0,0 +1,16 @@
+package br.com.exercicios;
+
+public class Bee2752 {
+    public static void main(String[] args) {
+        String frase = "AMO FAZER EXERCICIO NO URI";
+
+        System.out.printf("<%s>%n", frase);
+        System.out.printf("<%30s>%n", frase);
+        System.out.printf("<%.20s>%n", frase);
+        System.out.printf("<%-20s>%n", frase);
+        System.out.printf("<%-30s>%n", frase);
+        System.out.printf("<%.30s>%n", frase);
+        System.out.printf("<%30.20s>%n", frase);
+        System.out.printf("<%-30.20s>%n", frase);
+    }
+}
