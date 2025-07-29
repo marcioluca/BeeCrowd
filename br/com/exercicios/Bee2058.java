cb8d5d7f4a55238f40d73f8f26a37fa586be79d8
marcio lucas <lucasmrc2014@gmail.com>
2025-07-29 16:10:17 -0300

    resolving problem 2058

---
+++ br/com/exercicios/Bee2058.java
@@ -0,0 +1,13 @@
+package br.com.exercicios;
+
+import java.util.Scanner;
+// Triângulos e Polígonos Regulares
+public class Bee2058 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int nDetriangulos = n - 2;
+        System.out.println(nDetriangulos);
+        sc.close();
+    }
+}
