39dd2ef7f3056a33677d970e1a27bf430ed1aee1
marcio lucas <lucasmrc2014@gmail.com>
2025-07-30 07:50:51 -0300

    resolving problem 2679

---
+++ br/com/exercicios/Bee2679.java
@@ -0,0 +1,15 @@
+package br.com.exercicios;
+
+import java.util.Scanner;
+
+public class Bee2679 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        do { x++; }while (x % 2 != 0);
+
+        System.out.println(x);
+    }
+
+}
