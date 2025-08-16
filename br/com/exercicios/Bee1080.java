1f54a58a0e9419683f939bcf26ae691bab5c903b
Marcio Lucas <lucasmrc2014@gmail.com>
2025-08-16 19:40:41 -0300

    resolving problem 1080

--- br/com/exercicios/Bee1080.java
+++ br/com/exercicios/Bee1080.java
@@ -1,6 +1,19 @@
 package br.com.exercicios;
 import java.util.Scanner;
-// Maior e Posição
+
+/*
+    * Problema Bee 1080 - Maior e Posição
+    *
+    * Leia 100 valores inteiros. Apresente então o maior valor lido e a posição dentre os 100 valores lidos.
+    *
+    * Entrada
+    * A entrada contém 100 números inteiros, positivos e distintos.
+    *
+    * Saída
+    * O primeiro valor da saída é o maior dos 100 valores lidos. A segunda linha deve apresentar a posição do maior valor, considerando a primeira posição como 1 (um).
+ */
+
+
 public class Bee1080 {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
@@ -22,6 +35,7 @@ public class Bee1080 {
         }
         System.out.println(valorMaximo);
         System.out.println(posicaovalorMaximo);
+        s.close();
     }
 
 }
