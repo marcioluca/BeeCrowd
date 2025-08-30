fb1affb5fcc7f3b41c9aa63df0972ea9882c3834
Marcio Lucas <lucasmrc2014@gmail.com>
2025-08-29 21:31:12 -0300

    resolving problem 1075

--- br/com/exercicios/Bee1075.java
+++ br/com/exercicios/Bee1075.java
@@ -2,7 +2,18 @@ package br.com.exercicios;
 
 import java.util.Scanner;
 
-// Leia um valor inteiro N. Apresente todos os números entre 1 e 10000 que divididos por N dão resto igual a 2.
+/**
+ * Bee 1075 - Resto 2
+ *
+ * Problema: Dado um número inteiro N, imprimir todos os números entre 1 e 10000 que, quando divididos por N, deixam resto 2.
+ *
+ * Entrada:
+ * - Um número inteiro N (2 ≤ N ≤ 1000).
+ *
+ * Saída:
+ * - Todos os números entre 1 e 10000 que deixam resto 2 quando divididos por N, um por linha.
+ *
+ */
 public class Bee1075 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
@@ -13,5 +24,6 @@ public class Bee1075 {
                 System.out.println(i);
             }
         }
+        s.close();
     }
 }
